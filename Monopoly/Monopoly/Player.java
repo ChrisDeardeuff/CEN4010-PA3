947bb0c3475b6aa31947cc193fb9f4bb887b76ac
ChrisDeardeuff <chris.deardeuff1131@gmail.com>
2020-11-02 16:20:59 -0500

    Added file structure and Player class

---
+++ Monopoly/Monopoly/Player.java
@@ -0,0 +1,71 @@
+package Monopoly;
+
+public class Player {
+
+    Property[] propertiesOwned;
+    int balance;
+    boolean inPrison;
+    int position;
+
+    public Player(){
+
+        this.propertiesOwned = new Property[27];
+        this.balance = 1500;
+        this.inPrison = false;
+        this.position = 0;
+
+    }
+
+    public void movePlayerTo(int position){
+        //TODO
+    }
+
+    public void goToJail(){
+        this.inPrison = true;
+        this.position = 10;
+
+    }
+
+    public void addBalance(int money){
+        this.balance = this.balance + money;
+    }
+
+    public void subtractBalance(int money){
+        this.balance = this.balance - money;
+    }
+
+    public void removeProperty(Property property){
+        //TODO
+    }
+    public void addProperty(Property property){
+        //TODO
+    }
+
+    public Property[] getPropertiesOwned() {
+        return propertiesOwned;
+    }
+
+    public void setPropertiesOwned(Property[] propertiesOwned) {
+        this.propertiesOwned = propertiesOwned;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+    public boolean isInPrison() {
+        return inPrison;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void setPosition(int position) {
+        this.position = position;
+    }
+}
