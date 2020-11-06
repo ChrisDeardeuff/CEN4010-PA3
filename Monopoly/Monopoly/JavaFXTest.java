8fba7d30cb055e998d1b58acbed1e207f97c9d44
ChrisDeardeuff <chris.deardeuff1131@gmail.com>
2020-11-06 17:30:39 -0500

    JavaFX Test class

---
+++ Monopoly/Monopoly/JavaFXTest.java
@@ -0,0 +1,13 @@
+package Monopoly;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class JavaFXTest extends Application {
+
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+}
