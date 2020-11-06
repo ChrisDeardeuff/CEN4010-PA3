42d4cf6cbd92d7fcd69dd00629be8ad0d946722b
ChrisDeardeuff <chris.deardeuff1131@gmail.com>
2020-11-06 17:37:17 -0500

    More javaFX testing

--- Monopoly/Monopoly/JavaFXTest.java
+++ Monopoly/Monopoly/JavaFXTest.java
@@ -1,6 +1,8 @@
 package Monopoly;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class JavaFXTest extends Application {
@@ -9,5 +11,20 @@ public class JavaFXTest extends Application {
     @Override
     public void start(Stage stage) throws Exception {
 
+        BorderPane pane = new BorderPane();
+        Scene sceneStart = new Scene(pane, 780, 177);
+
+        // Set the stage title
+        stage.setTitle("Monopoly");
+        //Set Scene
+        stage.setScene(sceneStart);
+        //display the stage
+        stage.show();
+
+        stage.getScene().getRoot().requestFocus();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
