f784fad9a7b0bea9d8a6fccc0a45462f471e8d1f
rep467 <24.per.sander@gmail.com>
2020-11-14 18:25:45 -0500

    Added grid for property allignment

---
+++ PA3UI/PA3UI/Model/Property.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace PA3UI.ui
+{
+    public abstract class Property : UserControl
+    {
+        /// <summary>
+        /// Updates the visual part of properties on the board
+        /// </summary>
+        /// <param name="level">
+        /// Valid values for:
+        ///     Streets: -1 to 5
+        ///     RailRoad -1 to 0
+        ///     For the rest not valid
+        ///     This method has no checking if the value is valid and only updates the visual on the board</param>
+        public abstract void SetDevelopmentOfProperty(int level);
+    }
+}
