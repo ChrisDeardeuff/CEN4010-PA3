bd796ec20c2a6a2dbfe9ee5e6bc737b8d87fa827
ChrisDeardeuff <chris.deardeuff1131@gmail.com>
2020-11-15 00:50:45 -0500

    Ui Clean up and owned property implementations

---
+++ PA3UI/PA3UI/ViewModel/CardVeiwer.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace PA3UI.ui
+{
+    public partial class CardVeiwer : UserControl
+    {
+        public CardVeiwer()
+        {
+            InitializeComponent();
+        }
+    }
+}
