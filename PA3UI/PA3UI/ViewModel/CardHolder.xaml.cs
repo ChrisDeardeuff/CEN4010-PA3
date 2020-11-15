d7c20b87833923b2ef83d1e7fee41df6a45b5ce3
ChrisDeardeuff <chris.deardeuff1131@gmail.com>
2020-11-14 22:52:58 -0500

    Added Carholder UserControl

---
+++ PA3UI/PA3UI/ViewModel/CardHolder.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace PA3UI.ViewModel
+{
+    public partial class CardHolder : UserControl
+    {
+        public CardHolder()
+        {
+            InitializeComponent();
+        }
+    }
+}
