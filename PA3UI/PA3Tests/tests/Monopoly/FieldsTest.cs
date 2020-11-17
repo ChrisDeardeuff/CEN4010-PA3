0dd4d6f8b4ae5a30cbcc5e5ef94d771ff249593b
CD <chris.deardeuff1131@gmail.com>
2020-11-16 19:54:13 -0500

    Added example test.

---
+++ PA3UI/PA3Tests/tests/Monopoly/FieldsTest.cs
@@ -0,0 +1,21 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PA3BackEnd.src.Monopoly;
+
+namespace PA3Tests.tests.Monopoly
+{
+    [TestClass]
+    public class FieldTest
+    {
+        [TestMethod]
+        public void TestFieldsLocations()
+        {
+            var fields = new Fields();
+            
+            for (int i = 0; i < 40; i++)
+            {
+                Assert.AreEqual(fields.GetFieldAt(i).GetLocation(), i);
+            }
+        }
+    }
+}
