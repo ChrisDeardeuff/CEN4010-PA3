b54bf067caadc0a8126d0bded83771488a0a2e50
emilyjane99 <emily.ottesen@gmail.com>
2020-11-17 10:44:46 -0500

    railroads testing

---
+++ PA3UI/PA3Tests/tests/Monopoly/RailroadsTest.cs
@@ -0,0 +1,20 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PA3BackEnd.src.Monopoly;
+
+namespace PA3Tests.tests.Monopoly
+{
+    [TestClass]
+    public class RailroadsTest
+    {
+        [TestMethod]
+        public void CanBuildTest()
+        {
+            //var rails = new Railroads();
+           // Assert.Equals(false, rails.CanBeMortaged());
+            //Assert.Equals(false, rails.CanPlayerBuild());
+            
+        }
+        
+        
+    }
+}
