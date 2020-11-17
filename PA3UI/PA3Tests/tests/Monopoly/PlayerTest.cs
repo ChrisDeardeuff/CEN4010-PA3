de05644642d28c38936b0004b3da094a417bdf35
emilyjane99 <emily.ottesen@gmail.com>
2020-11-17 00:16:39 -0500

    working on player test

--- PA3UI/PA3Tests/tests/Monopoly/PlayerTest.cs
+++ PA3UI/PA3Tests/tests/Monopoly/PlayerTest.cs
@@ -1,7 +1,18 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PA3BackEnd.src.Monopoly;
+
 namespace PA3Tests.tests.Monopoly
 {
+    [TestClass]
     public class PlayerTest
     {
-        
+        [TestMethod]
+        public void MoveTest()
+        {
+            var player = new Player();
+           // player.movePlayerForward(2);
+            //Assert.AreEqual(player.position + 2, );
+        }
     }
 }
