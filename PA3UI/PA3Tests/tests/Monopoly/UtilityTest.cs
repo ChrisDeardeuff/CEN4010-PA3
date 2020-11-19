2ea495e2de521c5b9c34249a76f4319347e37c46
aaron5612 <55104863+aaron5612@users.noreply.github.com>
2020-11-19 15:13:49 -0500

    mortgage utility test

--- PA3UI/PA3Tests/tests/Monopoly/UtilityTest.cs
+++ PA3UI/PA3Tests/tests/Monopoly/UtilityTest.cs
@@ -18,5 +18,17 @@ namespace PA3Tests.tests.Monopoly
             Assert.IsFalse(ec.CanPlayerBuild(player1));
             Assert.IsFalse(ww.CanPlayerBuild(player1));
         }
+
+        [TestMethod]
+        public void CanMortgageTest()
+        {
+            var group = new Group(2);
+            var ec = new Utility(12, group, 150, "Electric Company");
+            var ww = new Utility(28, group, 150, "Water Works");
+            ec.DevelopProperty(-1);
+            ww.DevelopProperty(0);
+            Assert.IsFalse(ec.CanBeMortaged());
+            Assert.IsTrue(ww.CanBeMortaged());
+        }
     }
 }
