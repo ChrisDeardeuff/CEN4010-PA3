03c2c66af223fbf0c1d4666fb59541988d30afcc
aaron5612 <55104863+aaron5612@users.noreply.github.com>
2020-11-20 09:44:23 -0500

    single property mortgage test

--- PA3UI/PA3Tests/tests/Monopoly/PropertyTest.cs
+++ PA3UI/PA3Tests/tests/Monopoly/PropertyTest.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PA3BackEnd.src.Monopoly;
+
 
 namespace PA3Tests.tests.Monopoly
 {
@@ -7,8 +9,15 @@ namespace PA3Tests.tests.Monopoly
     public class PropertyTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void CanBeMortgagedTest()
         {
+            var group = new Group(1, 200);
+            Property property = new Street(37, group, 350, new int[] { 35, 175, 500, 1100, 1300, 1500 }, "Park Place");
+            Assert.IsTrue(property.CanBeMortaged());
+            property.DevelopProperty(-1);
+            Assert.IsFalse(property.CanBeMortaged());
+            property.DevelopProperty(1);
+            Assert.IsFalse(property.CanBeMortaged());
         }
     }
 }
