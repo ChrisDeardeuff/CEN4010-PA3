724cef485c100ea1267eb13cc9ab1911cfe601c6
emilyjane99 <emily.ottesen@gmail.com>
2020-11-21 18:02:34 -0500

    separate methods for railroads test

--- PA3UI/PA3Tests/tests/Monopoly/RailroadsTest.cs
+++ PA3UI/PA3Tests/tests/Monopoly/RailroadsTest.cs
@@ -7,7 +7,7 @@ namespace PA3Tests.tests.Monopoly
     public class RailroadsTest
     {
         [TestMethod]
-        public void RailroadTest()
+        public void RailroadRentTest()
         {
             var group = new Group(4);
             var rail1 = new Railroads(15, group, 200, "Pennsylvania Railroad");
@@ -15,25 +15,44 @@ namespace PA3Tests.tests.Monopoly
             var rail3 = new Railroads(25, group, 200, "B & 0 Railroad");
             var rail4 = new Railroads(35, group, 200, "Short Line");
             
+            //get rent when one railroad is owned
             rail1.BoughtByPlayer(0);
-            //test can be mortgaged and can player build
-            Assert.AreEqual(true, rail1.CanBeMortaged()); //separate method for this
-            //get rent when railroad is mortgaged
             Assert.AreEqual(25, rail1.GetRent());
+            //get rent when railroad is mortgaged
             rail1.DevelopProperty(-1);
             Assert.AreEqual(0, rail1.GetRent());
-            //get rent when one railroad is owned
-            rail1.DevelopProperty(0);
-            Assert.AreEqual(25, rail1.GetRent());
             //get rent with two railroads
+            rail1.DevelopProperty(0);
             rail2.BoughtByPlayer(0);
             Assert.AreEqual(50, rail1.GetRent());
             //get rent with 3 railroads
             rail3.BoughtByPlayer(0);
             Assert.AreEqual(100, rail1.GetRent());
-            rail4.BoughtByPlayer(0);
             //get rent with 4 railroads
+            rail4.BoughtByPlayer(0);
             Assert.AreEqual(200, rail1.GetRent());
+        }
+
+        [TestMethod]
+        public void RailMortgageTest()
+        {
+            //testing can be mortgaged
+            var group = new Group(1);
+            var rail1 = new Railroads(15, group, 200, "Pennsylvania Railroad");
+            //test can be mortgaged and can player build
+            Assert.AreEqual(true, rail1.CanBeMortaged()); //separate method for this
+        }
+
+        [TestMethod]
+
+        public void CanBuildOnRailroadTest()
+        {
+            //testing can player build
+            var player = new Player();
+            var group = new Group(1);
+            var rail1 = new Railroads(15, group, 200, "Pennsylvania Railroad");
+            //test can be mortgaged and can player build
+            Assert.AreEqual(false, rail1.CanPlayerBuild(0)); //separate method for this
         }
     }
 }
