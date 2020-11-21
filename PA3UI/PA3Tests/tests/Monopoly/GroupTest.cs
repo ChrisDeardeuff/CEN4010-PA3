288811453f65a9013dedb5c196caaafbd53c9618
emilyjane99 <emily.ottesen@gmail.com>
2020-11-21 17:12:48 -0500

    tested group test

--- PA3UI/PA3Tests/tests/Monopoly/GroupTest.cs
+++ PA3UI/PA3Tests/tests/Monopoly/GroupTest.cs
@@ -12,25 +12,34 @@ namespace PA3Tests.tests.Monopoly
         public void GroupPropertyTest()
         {
             var player = new Player();
-            var player2 = new Player();
+         //   var player2 = new Player();
             var group = new Group(3);
-            //test get amount owns with none
+            //test get amount player owns with none
             Assert.ThrowsException<NullReferenceException>(()=>group.GetAmountPlayerOwns(0));
-           
-            var street1 = new Street(6, new Group(2, 50), 100, new int[] {6, 30, 90, 270, 400, 550}, "Oriental Ave");
-            var street2 = new Street(8, new Group(2,50), 100, new int[] { 6, 30, 90, 270, 400, 550 }, "Vermont Ave");
-            var street3 = new Street(9, new Group(2, 50), 120, new int[] {8, 40, 100, 300, 450, 600},
-                "Connecticut Ave");
-            //test add property
-            group.AddProperty(street1);
+            var street1 = new Street(6,group, 100, new int[] {6, 30, 90, 270, 400, 550}, "Oriental Ave");
+            var street2 = new Street(8, group, 100, new int[] { 6, 30, 90, 270, 400, 550 }, "Vermont Ave");
+            var street3 = new Street(9, group, 120, new int[] {8, 40, 100, 300, 450, 600}, "Connecticut Ave");
+            
+            
+         //   group.AddProperty(street1);
             //test 'has any buildings' with no buildings
-            Assert.ThrowsException<NullReferenceException>(()=>group.HasAnyBuildings());
-            //test get amount owns with 1 
+         //   Assert.ThrowsException<NullReferenceException>(()=>group.HasAnyBuildings());
+            //test get amount player owns with 1 
+            Assert.AreEqual(0,group.GetAmountPlayerOwns(0));
+            street1.BoughtByPlayer(0);
+          //  street1.DevelopProperty(0);
+          //  player.addProperty(street1);
             Assert.AreEqual(1,group.GetAmountPlayerOwns(0));
-            group.AddProperty(street2);
+            //test get amount player owns with 2
+         //   group.AddProperty(street2);
+           // street2.DevelopProperty(0);
+            street2.BoughtByPlayer(0);
             Assert.AreEqual(2,group.GetAmountPlayerOwns(0));
-            street1.DevelopProperty(1);
             //test 'has any buildings' with a building
+            street3.BoughtByPlayer(0);
+            Assert.AreEqual(3,group.GetAmountPlayerOwns(0));
+            
+            street1.DevelopProperty(1);
             Assert.AreEqual(true,group.HasAnyBuildings());
         }
     }
--- PA3UI/PA3Tests/tests/Monopoly/RailroadsTest.cs
+++ PA3UI/PA3Tests/tests/Monopoly/RailroadsTest.cs
@@ -14,30 +14,25 @@ namespace PA3Tests.tests.Monopoly
             var rail2 = new Railroads(5, group, 200, "Reading Railroad");
             var rail3 = new Railroads(25, group, 200, "B & 0 Railroad");
             var rail4 = new Railroads(35, group, 200, "Short Line");
-            //var player = new Player();
+            
             rail1.BoughtByPlayer(0);
-           // player.addProperty(rails);
             //test can be mortgaged and can player build
             Assert.AreEqual(true, rail1.CanBeMortaged()); //separate method for this
-           // Assert.AreEqual(false, rail.CanPlayerBuild(0));
             //get rent when railroad is mortgaged
             Assert.AreEqual(25, rail1.GetRent());
             rail1.DevelopProperty(-1);
             Assert.AreEqual(0, rail1.GetRent());
             //get rent when one railroad is owned
-           // player.addProperty(rail);
-           //get rent with two railroads
             rail1.DevelopProperty(0);
             Assert.AreEqual(25, rail1.GetRent());
+            //get rent with two railroads
             rail2.BoughtByPlayer(0);
             Assert.AreEqual(50, rail1.GetRent());
             //get rent with 3 railroads
-         //   player.addProperty(rail3);
             rail3.BoughtByPlayer(0);
             Assert.AreEqual(100, rail1.GetRent());
             rail4.BoughtByPlayer(0);
             //get rent with 4 railroads
-          //  player.addProperty(rail4);
             Assert.AreEqual(200, rail1.GetRent());
         }
     }
