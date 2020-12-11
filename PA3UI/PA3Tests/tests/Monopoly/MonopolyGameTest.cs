555dee9a32c46189119138374871067bd634ee7a
ChrisDeardeuff <chris.deardeuff1131@gmail.com>
2020-12-11 14:51:55 -0500

    NextPlayersTurnTest -Emily

--- PA3UI/PA3Tests/tests/Monopoly/MonopolyGameTest.cs
+++ PA3UI/PA3Tests/tests/Monopoly/MonopolyGameTest.cs
@@ -23,7 +23,29 @@ namespace PA3Tests.tests.Monopoly
         [TestMethod]
         public void NextPlayersTurnTest()
         {
+            MonopolyGame mg = new MonopolyGame(2);
+
+            var roll = mg.CanRoleDice;
+            Assert.AreEqual(false, roll);
             
+            //round 1, player turn 1
+            mg.NextPlayersTurn();
+            roll = mg.CanRoleDice;
+            Assert.AreEqual(true,roll);
+            var currentPlayer = mg.currentPlayerID;
+            Assert.AreEqual(1, currentPlayer);
+            var round = mg.Round;
+            Assert.AreEqual(1, round);
+            //player 2
+            mg.NextPlayersTurn();
+            currentPlayer = mg.currentPlayerID;
+            Assert.AreEqual(2, currentPlayer);
+            //round 2 player 1
+            mg.NextPlayersTurn();
+            currentPlayer = mg.currentPlayerID;
+            Assert.AreEqual(1, currentPlayer);
+            round = mg.Round;
+            Assert.AreEqual(2, round);
         }
 
         [TestMethod]
